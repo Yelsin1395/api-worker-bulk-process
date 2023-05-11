4f142610f6a11ba37d00d70c6cb997948a01b955
Jesús Yelsin Broly <yelgari13@gmail.com>
2023-05-11 12:37:17 -0500

    other: refactor process for query to datbase clinicarecord.

--- src/container/container.js
+++ src/container/container.js
@@ -6,12 +6,10 @@ import config from './config';
 import server from './server';
 import CosmosImpl from './cloudConnectionCosmos.db';
 import routes from '../routes/index';
-import APP_DATA_PROCESS from '../../APP_DATA_PROCESS.json'
 
 const container = createContainer();
 container.register({
   pkg: asValue(pkg),
-  appDataProcess: asValue(APP_DATA_PROCESS),
   config: asValue(config),
   server: asClass(server).singleton(),
   cosmosImpl: asClass(CosmosImpl).singleton(),
--- src/services/invoice.service.js
+++ src/services/invoice.service.js
@@ -1,48 +1,54 @@
 import { Worker } from 'worker_threads';
-import runner from '../common/runner';
+import fs from 'fs';
 
 export default class InvoiceService {
-  constructor({ appDataProcess, clinicaRecordRepository, documentRepository }) {
-    this._appDataProcess = appDataProcess;
+  constructor({ clinicaRecordRepository, documentRepository }) {
     this._clinicaRecordRepository = clinicaRecordRepository;
     this._documentRepository = documentRepository;
   }
 
   async processCrossInvoice() {
-    let processEnd = false;
-    const processGenerator = runner(this._appDataProcess, 20);
+    let continuationToken = null;
 
     do {
-      const { done, value } = processGenerator.next();
-      if (value) {
-        const emitData = [];
+      const result = await this._clinicaRecordRepository.traverse(continuationToken);
 
-        for (const itemProcess of value) {
-          console.log(`🚀 The item will be processed: ${JSON.stringify(itemProcess)}`);
+      continuationToken = result.continuationToken;
+      fs.appendFileSync('logToken.txt', continuationToken);
 
-          const clinicaRecord = await this._clinicaRecordRepository.getRecordByLoteAndFactura(itemProcess.nroLote, itemProcess.facturaNro);
+      const emitData = [];
 
-          console.log(`📄 Total documents clinicaRecord obtained: ${clinicaRecord.length}`);
-
-          if (!clinicaRecord.length) {
-            console.log(`🚫 Clinica record it does not have any record, we continue with the following query.`);
-            continue;
-          }
-
-          const document = await this._documentRepository.getRecordByLoteAndFactura(itemProcess.nroLote, itemProcess.facturaNro);
-
-          console.log(`📄 Total documents obtained: ${document.length}`);
-
-          if (!document.length) {
-            console.log('🚫 Document it does not have any record, we continue with the following query.');
-            continue;
-          }
-
-          console.log('❤️ Data clínica record match with data document, adding data to memory');
-
-          emitData.push({ clinicaRecord, document });
+      for (const itemProcess of result.resources) {
+        if (typeof itemProcess.nroLote === 'string') {
+          itemProcess.nroLote = parseInt(itemProcess.nroLote);
         }
 
+        console.log(`🚀 The item will be processed: ${JSON.stringify(itemProcess)}`);
+
+        const clinicaRecord = await this._clinicaRecordRepository.getRecordByLoteAndFactura(itemProcess.nroLote, itemProcess.facturaNro);
+
+        console.log(`📄 Total documents clinicaRecord obtained: ${clinicaRecord.length}`);
+
+        if (!clinicaRecord.length) {
+          console.log(`🚫 Clinica record it does not have any record, we continue with the following query.`);
+          continue;
+        }
+
+        const document = await this._documentRepository.getRecordByLoteAndFactura(itemProcess.nroLote, itemProcess.facturaNro);
+
+        console.log(`📄 Total documents obtained: ${document.length}`);
+
+        if (!document.length) {
+          console.log('🚫 Document it does not have any record, we continue with the following query.');
+          continue;
+        }
+
+        console.log('❤️ Data clínica record match with data document, adding data to memory');
+
+        emitData.push({ clinicaRecord, document });
+      }
+
+      if (emitData.length) {
         const dataProcess = Buffer.from(JSON.stringify(emitData), 'utf8');
 
         console.log('📨 Send data process...');
@@ -54,7 +60,6 @@ export default class InvoiceService {
           console.log(`♻️ Worker cross invoice in process ${processId}`);
         });
       }
-      processEnd = done;
-    } while (!processEnd);
+    } while (continuationToken);
   }
 }
